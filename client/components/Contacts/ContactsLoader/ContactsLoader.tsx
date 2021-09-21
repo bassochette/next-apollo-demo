fc925911d38eb9f5bdbeb779ae398d84c31ab281
Julien Prugne <julien@webeleon.dev>
2021-09-21 19:14:53 +0200

    feat(client): skeleton loader for cards

--- client/components/Contacts/ContactsLoader/ContactsLoader.tsx
+++ client/components/Contacts/ContactsLoader/ContactsLoader.tsx
@@ -1,5 +1,33 @@
 import { FunctionComponent } from "react";
+import * as styles from "./contactsLoader.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAddressBook,
+  faEnvelopeSquare,
+  faPhoneSquareAlt,
+} from "@fortawesome/free-solid-svg-icons";
+
+const ContactSkeleton: FunctionComponent = () => (
+  <div className={styles["card-container"]}>
+    <div className={styles["field"]}></div>
+    <div className={styles["field"]}>
+      <FontAwesomeIcon icon={faPhoneSquareAlt} />
+    </div>
+    <div className={styles["field"]}>
+      <FontAwesomeIcon icon={faEnvelopeSquare} />
+    </div>
+    <div className={styles["field"]}>
+      <FontAwesomeIcon icon={faAddressBook} />
+    </div>
+  </div>
+);
 
 export const ContactsLoader: FunctionComponent = () => {
-  return <>contacts loading</>;
+  return (
+    <div className={styles["loader"]}>
+      <ContactSkeleton />
+      <ContactSkeleton />
+      <ContactSkeleton />
+    </div>
+  );
 };
