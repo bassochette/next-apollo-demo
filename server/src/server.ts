3bdcd26202b522726f587675671f8ba2794cf7a4
Julien Prugne <julien@webeleon.dev>
2021-09-21 11:04:14 +0200

    fix: add sandbox link on startup

--- server/src/server.ts
+++ server/src/server.ts
@@ -36,7 +36,8 @@ graphqlServer.start().then(() => {
   const port = process.env.PORT || 5000;
   app.listen(port, () => {
     console.log(
-      `Graphql Server started on: http://localhost:${port}${graphqlServer.graphqlPath}`
+      `Graphql Server started on: http://localhost:${port}${graphqlServer.graphqlPath}
+Graphql playground: https://studio.apollographql.com/sandbox/explorer/`
     );
   });
 });
