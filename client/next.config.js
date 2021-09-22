c2dffa1fe03e4f4dc55fbf6fe22092878c83125f
Julien Prugne <julien@webeleon.dev>
2021-09-22 10:07:45 +0200

    chore(client): add a few security headers

---
+++ client/next.config.js
@@ -0,0 +1,30 @@
+const securityHeaders = [
+  {
+    key: "X-DNS-Prefetch-Control",
+    value: "on",
+  },
+  {
+    key: "X-XSS-Protection",
+    value: "1; mode=block",
+  },
+  {
+    key: "X-Frame-Options",
+    value: "SAMEORIGIN",
+  },
+  {
+    key: "Referrer-Policy",
+    value: "origin-when-cross-origin",
+  },
+];
+
+module.exports = {
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
+};
